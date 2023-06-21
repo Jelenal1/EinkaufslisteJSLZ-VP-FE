e76181f2083442da748375ea9cb371d475c94bb3
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2023-06-21 15:17:16 +0200

    style: did some basic styling

--- einkaufsliste-frontend/src/Components/IndividualTask.tsx
+++ einkaufsliste-frontend/src/Components/IndividualTask.tsx
@@ -1,4 +1,14 @@
 import { useState } from 'react'
+
+const style = {
+  taskItem: {
+    wrapper: 'bg-slate-500 text-white flex items-center p-3 rounded-md w-fit m-2',
+    checkbox: 'mr-2 aspect-square w-6',
+    title: 'mr-2',
+    created_at: 'mr-2',
+  },
+}
+
 interface Task {
   id: number;
   title: string;
@@ -20,12 +30,12 @@ function IndividualTask({ task, onUpdate, onDelete }: TaskProps) {
     setEditId(task.id);
   }
   return (
-    <div>
+    <div className={style.taskItem.wrapper}>
+      <input type="checkbox" defaultChecked={task.status} className={style.taskItem.checkbox} />
+      <h1 className={style.taskItem.title}>{task.title}</h1>
+      <span className={style.taskItem.created_at}>{new Date(task.created_at).toLocaleDateString()}</span>
       <button onClick={() => handleEditClick()}>✏️</button >
       <button onClick={() => onDelete(task.id)}>🗑️</button>
-      <input type="checkbox" defaultChecked={task.status} />
-      <h1>{task.title}</h1>
-      <span>{new Date(task.created_at).toLocaleDateString()}</span>
     </div>
   )
 } export default IndividualTask;
