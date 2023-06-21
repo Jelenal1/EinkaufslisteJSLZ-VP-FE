02158326a8f3a1a742ac34f6ea1dfe09322d860c
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2023-06-21 14:50:03 +0200

    feat: the core of the item component

--- einkaufsliste-frontend/src/Components/IndividualTask.tsx
+++ einkaufsliste-frontend/src/Components/IndividualTask.tsx
@@ -1,9 +1,9 @@
-
+import { useState } from 'react'
 interface Task {
   id?: number;
   title: string;
   status: boolean;
-  created_at?: number;
+  created_at: number;
   fk_user_id: number;
 }
 
@@ -15,10 +15,16 @@ interface TaskProps {
 }
 
 function IndividualTask({ task, onUpdate, onDelete }: TaskProps) {
+  const [editId, setEditId] = useState<number | null>();
+  const handleEditClick = () => {
+    setEditId(task.id);
+  }
   return (
     <div>
+      <button onClick={() => handleEditClick()}>✏️</button >
+      <input type="checkbox" defaultChecked={task.status} />
       <h1>{task.title}</h1>
+      <span>{new Date(task.created_at).toLocaleDateString()}</span>
     </div>
-
   )
 } export default IndividualTask;
