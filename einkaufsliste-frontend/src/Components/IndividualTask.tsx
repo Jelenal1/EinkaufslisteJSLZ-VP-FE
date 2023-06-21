faf177cfdeb4fee966743e1ce4e1ce29f7a7637e
luczwyssig <77100623+thefaceslayer12@users.noreply.github.com>
2023-06-21 13:30:31 +0200

    feature: Grundstruktur der App erstellt, mit den Verschiedenen Komponenten.

---
+++ einkaufsliste-frontend/src/Components/IndividualTask.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+interface Task {
+
+  id: number;
+  title: string;
+  status: boolean;
+  created_at: number;
+  fk_user_id: number;
+}
+
+
+interface TaskProps {
+  task: Task;
+  onUpdate: (updatedTask: Task) => void;
+  onDelete: (deletedTask: Task) => void;
+}
+
+function IndividualTask({ task, onUpdate, onDelete }: TaskProps) {
+  return(
+    <div>
+      <h1>{task.title}</h1>
+    </div>
+
+  )
+} export default IndividualTask;
---
+++ einkaufsliste-frontend/src/Pages/App.tsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import axios from 'axios'
+import Tasklist from '../Components/Tasklist';
+
+interface Task {
+
+  id: number;
+  title: string;
+  status: boolean;
+  created_at: number;
+  fk_user_id: number;
+}
+
+function App() {
+  const [tasks, settasks] = useState<Task[]>([])
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  async function fetchTasks () {
+    try{
+      
+    }
+    catch {
+
+    }
+  }
+
+  async function updateTasks() {
+    try{
+
+    }
+    catch {
+
+    }
+  }
+
+  async function deleteTask() {
+    try{
+
+    }
+    catch{
+      
+    }
+  }
+
+
+  return (
+    <div>
+      <Tasklist tasks={tasks} onUpdate={updateTasks} onDelete={deleteTask}/>
+
+
+    </div>
+  )
+}
+
+export default App
