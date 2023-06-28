e939c48dc00e48886f571dee0ff1f332b21c21f9
luczwyssig <77100623+thefaceslayer12@users.noreply.github.com>
2023-06-28 15:17:32 +0200

    fix: fixed cross origin problems.

--- einkaufsliste-frontend/src/Pages/App.tsx
+++ einkaufsliste-frontend/src/Pages/App.tsx
@@ -21,61 +21,112 @@ function App() {
     }
   ])
 
-  useEffect(() => {
-    fetchTasks();
-  }, []);
+
+
+  /*async function login() {
+    try {
+      const response = await axios.post('http://localhost:3000/login', {
+        withCredentials: true,
+        code: 12345
+      });
+  
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }*/
+
+  async function login() {
+    try {
+      const response = await fetch('http://localhost:3000/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include', // Include cookies
+        body: JSON.stringify({ code: 12345 }),
+      });
+  
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+  
+        // Set cookie from response
+        const cookie = data.cookie;
+        if (cookie) {
+          document.cookie = `connect.sid=${cookie}`;
+        }
+      } else if (response.status === 401) {
+        console.log('Unauthorized');
+      } else {
+        console.error('Login failed', response);
+      }
+    } catch (error) {
+      console.error('Login failed', error);
+    }
+  }
 
   async function fetchTasks() {
     try {
-
-      const response = await axios.get<Task[]>("the backend string");
-      const fetchedTasks = response.data;
-      settasks(fetchedTasks);
-
-
-    }
-    catch (error) {
-      console.error("couldnt fetch", error)
+      const response = await fetch('http://localhost:3000/items', {
+        method: 'GET',
+        credentials: 'include', // Include cookies
+      });
+      if (response.ok) {
+        const fetchedTasks = await response.json();
+        console.log(fetchedTasks);
+      } else if (response.status === 401) {
+        console.log('Unauthorized');
+      } else {
+        console.error('Could not fetch', response);
+      }
+    } catch (error) {
+      console.error('Could not fetch', error);
     }
   }
-
-
-  async function updateTasks(updatedTask: Task) {
-
+  
+  async function updateTasks(updatedTaskId: number) {
     try {
-      await axios.patch<Task>("the backend string", updatedTask);
+      await axios.patch(`http://localhost:3000/items/${updatedTaskId}`, null, {
+        withCredentials: true, // Include cookies
+      });
       fetchTasks();
-
-
-    }
-    catch (error) {
-      console.error("couldnt update", error)
+    } catch (error) {
+      console.error("Couldn't update", error);
     }
   }
-
+  
   async function deleteTask(deleteTaskId: number) {
-
     try {
-      await axios.delete("backend string/deleteTaskId");
+      await axios.delete(`http://localhost:3000/items/${deleteTaskId}`, {
+        withCredentials: true, // Include cookies
+      });
       fetchTasks();
-    }
-    catch (error) {
-      console.error("couldnt delete", error);
+    } catch (error) {
+      console.error("Couldn't delete", error);
     }
   }
-
-
+  
   async function postTask(addedTask: Task) {
     try {
-      await axios.post<Task>("the backend string", addedTask);
+      await axios.post<Task>('http://localhost:3000/items', addedTask, {
+        withCredentials: true, // Include cookies
+      });
       fetchTasks();
-    }
-    catch (error) {
-      console.error("couldnt post", error);
-
+    } catch (error) {
+      console.error("Couldn't post", error);
     }
   }
 
+  useEffect(() => {
+    login();
+    setTimeout(() => {
+      fetchTasks();
+    }, 1000);
+    
+    
+    
+  }, []); 
 
   return (
 
