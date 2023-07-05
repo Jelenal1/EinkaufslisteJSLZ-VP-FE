c9b0d9fd800c1da826bd4cd3e9d722823b54e71a
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2023-07-05 13:41:31 +0200

    style: login form

--- einkaufsliste-frontend/src/Pages/Login.tsx
+++ einkaufsliste-frontend/src/Pages/Login.tsx
@@ -10,13 +10,12 @@ export default function Login({ login }: { login: (password: number) => void}) {
                     login(parseInt(event.target.groupid.value));
                 }
             }
-                className="flex flex-col max-w-xl gap-2 text-center"
-            >
+                className="flex flex-col max-w-xl gap-2 text-center mx-auto mt-32 border bg-slate-400 rounded-lg p-2">
                 <label htmlFor="groupid">Join Group with ID</label>
-                <input type="text" id="groupid" name="groupid" className="border border-gray-400" />
+                <input type="text" id="groupid" name="groupid" className="rounded-xl" />
                 <label htmlFor="newgroupid">Create a new Group</label>
-                <input type="text" id="newgroupid" className="border border-gray-400" />
-                <input type="submit" value="Login" />
+                <input type="text" id="newgroupid" className="rounded-2xl" />
+                <input type="submit" value="Login" className="rounded-xl bg-slate-200 w-20 mx-auto" />
             </form>
         </div>
     )
